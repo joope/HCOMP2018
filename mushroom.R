cb82adf956e251f9ae6a016afc1cedac72a462ba
Milla Peltoniemi <tunturikissa@gmail.com>
2018-04-09 17:24:32 +0300

    refactor the mushroom code

--- mushroom.R
+++ mushroom.R
@@ -1,73 +1,103 @@
 #https://rpubs.com/hcrews47/mushroom
 #Mushroom data contains observations from 23 different mushrooms
 
-getCleanMushroomData <- function(mushrooms){
-  mushrooms <- read.csv("./agaricus-lepiota.data.txt")
-  #remove rows including ? values
-  mushrooms[mushrooms=="?"] <-NA
-  mushrooms <- mushrooms[complete.cases(mushrooms), ]
-  
-  #export labels from the data, 1 if edible, 0 if poisonous
-  mushrooms$class = sapply(mushrooms$p, function(x){ifelse(x=='e', 1, 0)})
-  mushrooms$p <- NULL
-  return(mushrooms)
+
+# load mushroom data
+mushrooms_original <- read.csv("data/mushroom.csv")
+library(data.table)
+N<-length(mushrooms_original[,1])
+head(mushrooms_original)
+
+#load 2n data
+
+#load 3rd data
+
+#DATA PREPROCESSING FOR CREATING MULTIPLE WORKERS
+# IDs ; labels = (1,2) 1-eatable, 2 - poisonous
+
+mushrooms=data.table(ID=c(1:N), label=c(mushrooms_original$class))
+head(mushrooms)
+mushrooms$label
+
+#SIMULATE NOISY DATA (30% noise) FOR 10 WORKERS. 
+
+#function to simulate noisy labels
+simulateNoisyWorkers <- function(amount_of_workers, amount_of_noise, expertlabel){
+  workers_with_noisy_labels<-matrix(NA,nrow=N, ncol=amount_of_workers)
+  for (i in 1:amount_of_workers) {
+    noise <- rbinom(N,1,amount_of_noise)
+    labels <- expertlabel
+    labels[expertlabel==1 & noise] <- 2
+    labels[expertlabel==2 & noise] <- 1
+    workers_with_noisy_labels[,i] <- labels
+  }
+  return(workers_with_noisy_labels)
 }
 
 
-simulateNoisyDataset <- function(dataset, workerAccuracy){
-  simulated = dataset
-  simulated$noise <- rbinom(nrow(dataset), 1, workerAccuracy)
+workers_labels<-simulateNoisyWorkers(10,0.3,mushrooms$label)
+
+#confirm 70% labels are indetical with expert labels
+res <- workers_labels == mushrooms$label
+length(res[res==TRUE])/(10*nrow(res))
+
+#------------------------------
+
+#MAJOTRITY VOTING to reduce multiple labels
+#install.packages("mclust")
+library(mclust) 
+
+# apply the majority voting to worker labels
+majorityVotingForLabels <- function(workers_labels){
+  N <- nrow(workers_labels)
+  labels_majorityvoting<-rep(0, times=N)
+  for (i in 1:N) {
+    labels_majorityvoting[i]<-(majorityVote(workers_labels[i,]))$majority
+  }
+  
+  #combine majority voted labels with original mushroom data
+  labels_majorityvoting <- sapply(labels_majorityvoting, function(x){ifelse(x== "2", 'p', 'e')})
+  labels_majorityvoting <- as.factor(labels_majorityvoting)
+  
+  return(labels_majorityvoting)
+}  
+
+labels_majorityvoting <- majorityVotingForLabels(workers_labels)
+mushrooms_majorityvotinglabels<-cbind(labels_majorityvoting, mushrooms_original[,-1])
+head(mushrooms_majorityvotinglabels)
+names(mushrooms_majorityvotinglabels)[names(mushrooms_majorityvotinglabels) == "labels_majorityvoting"] = "class" 
+
+#USE GOLDEN LABELS to reduce multiple labels
+
+goldenLabelCheckForLabels <- function(workers_labels){
+  #TODO
+  return(NULL)
+}
+
+
+#------------------------------
+
+#PARTITION TO TEST AND TRAIN DATA
+
+#-----------------
+
+#RUN 3 DIFFERENT CLASSIFICATION ALGORITHMS
+
+
+
+# simulateNoisyDataset <- function(dataset, workerAccuracy){
+#  simulated = dataset
+#  simulated$noise <- rbinom(nrow(dataset), 1, workerAccuracy)
   
   # Simulate noise to classes. If noise == 0 swap classes
-  simulated$class[dataset$class == 1 & simulated$noise == 0] = 0
-  simulated$class[dataset$class == 0 & simulated$noise == 0] = 1
-  simulated$class[dataset$class == 1 & simulated$noise == 1] = 1
-  simulated$class[dataset$class == 0 & simulated$noise == 1] = 0
+#  simulated$class[dataset$class == 1 & simulated$noise == 0] = 0
+#  simulated$class[dataset$class == 0 & simulated$noise == 0] = 1
+#  simulated$class[dataset$class == 1 & simulated$noise == 1] = 1
+#  simulated$class[dataset$class == 0 & simulated$noise == 1] = 0
   
-  simulated$class = factor(simulated$class)
-  simulated$noise <- NULL
+#  simulated$class = factor(simulated$class)
+#  simulated$noise <- NULL
   
-  return(simulated)
-}
+#  return(simulated)
+#}
 
-data <- getCleanMushroomData()
-
-# create test and train sets with some ratio
-smp_size <- floor(0.7 * nrow(dataset))
-
-# set the seed for reproductibility
-set.seed(123)
-train_ind <- sample(seq_len(nrow(dataset)), size = smp_size)
-train <- dataset[train_ind, ]
-test <- dataset[-train_ind, ]
-
-trainSimulated <- simulateNoisyDataset(train, 0.95)
-
-# Visualize simulated errors
-ifelse(train$class == trainSimulated$class, 1, 0)
-
-#desicion tree
-library(rpart)
-#train tree
-tree = rpart(class~., data=trainSimulated, control = rpart.control(cp = .0005)) 
-#test prediction with test data set
-tree_pred = predict(tree, test, type='class')
-mean(tree_pred==test$class)
-table(tree_pred, test$class)
-
-#prediction with original data set
-tree_pred_full = predict(tree, mushrooms, type='class')
-mean(tree_pred_full==mushrooms$class)
-table(tree_pred_full, mushrooms$class)
-
-##kNN
-library(class)
-#not working at the moment
-#knn(train, test, train$class, k=3)
-
-#SVM
-library(e1071)
-#not working at the moment
-svm_model <- svm(class~., data=train, type='C-classification', kernel='radial')
-pred_train <-predict(svm_model,train) 
-mean(pred_train==train$class) 
