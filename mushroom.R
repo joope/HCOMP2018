ea0c2ba0ddb3e851a4d15ff9875862ffb4ab2bef
Milla Peltoniemi <tunturikissa@gmail.com>
2018-04-10 00:41:43 +0300

    add data partitioning for test and train datasets

--- mushroom.R
+++ mushroom.R
@@ -12,7 +12,7 @@ data1 <- mushrooms_original
 mushrooms=data.table(ID=c(1:N), label=c(mushrooms_original$class))
 data1_expertlabels <- mushrooms$label
 
-#load 2n data
+#load 2nd data
 library(datasets)
 data(iris)
 head(iris)
@@ -20,8 +20,6 @@ data2 <- iris
 
 iris_flowers=data.table(ID=c(1:nrow(iris)), label=c(iris$Species))
 data2_expertlabels <- iris_flowers$label
-
-unique(data2_expertlabels)
 
 #load 3rd data
 #TODO
@@ -36,10 +34,10 @@ simulateNoisyWorkers <- function(amount_of_workers, amount_of_noise, expertlabel
   workers_with_noisy_labels<-matrix(NA,nrow=N, ncol=amount_of_workers)
   for (i in 1:amount_of_workers) {
     #generates which labels are noisy for each worker
-    noise <- rbinom(N,1,amount_of_noise) # 0,1,1 etc
+    noise <- rbinom(N,1,1-amount_of_noise) # 0,1,1 etc
     labels <- expertlabel
     for(j in 1:N){
-      # 1 == noisy label
+      # 1 == label with noise
       if(noise[j] == 1){
         #replace the chosen noisy labels with incorret values
         wrong_labels_for_row_j <- label_values[which(label_values != expertlabel[j])]
@@ -53,16 +51,17 @@ simulateNoisyWorkers <- function(amount_of_workers, amount_of_noise, expertlabel
   return(workers_with_noisy_labels)
 }
 
+noise_level <- 0.3
+# incorret labels 30%
+data1_workers_labels<-simulateNoisyWorkers(1,noise_level,data1_expertlabels)
+data2_workers_labels<-simulateNoisyWorkers(1,noise_level,data2_expertlabels)
 
-data1_workers_labels<-simulateNoisyWorkers(1,0.7,data1_expertlabels)
-data2_workers_labels<-simulateNoisyWorkers(1,0.7,data2_expertlabels)
-
-#confirm 70% labels are indetical with expert labels
+#confirm 70% labels are indetical/correct with expert labels
 res <- data1_workers_labels == data1_expertlabels
-length(res[res==FALSE])/(ncol(res)*nrow(res))
+length(res[res==TRUE])/(ncol(res)*nrow(res))
 
 res <- data2_workers_labels == data2_expertlabels
-length(res[res==FALSE])/(ncol(res)*nrow(res))
+length(res[res==TRUE])/(ncol(res)*nrow(res))
 
 #------------------------------
 
@@ -81,7 +80,6 @@ majorityVotingForLabels <- function(workers_labels){
 }  
 
 #CONSENSUS ALG 2: USE GOLDEN LABELS to reduce multiple labels with majority voting
-
 goldenLabelCheckForLabels <- function(workers_labels){
   #TODO
   return(NULL)
@@ -95,17 +93,50 @@ data1_labels_majorityvoting <- as.factor(data1_labels_majorityvoting)
 mushrooms_majorityvotinglabels<-cbind(data1_labels_majorityvoting, data1[,-1])
 names(mushrooms_majorityvotinglabels)[names(mushrooms_majorityvotinglabels) == "data1_labels_majorityvoting"] = "class" 
 
+#combine golden label majority voted labels with original mushroom data
+#TODO
+
 #combine majority voted labels with original iris data
 data2_labels_majorityvoting <- majorityVotingForLabels(data2_workers_labels)
 data2_labels_majorityvoting <- as.factor(data2_labels_majorityvoting)
 iris_majorityvotinglabels<-cbind(data2_labels_majorityvoting, data2[,-5])
 names(iris_majorityvotinglabels)[names(iris_majorityvotinglabels) == "data2_labels_majorityvoting"] = "class" 
 
-
+#combine golden label majority voted labels with original iris data
+#TODO
 
 #------------------------------
 
 #PARTITION TO TEST AND TRAIN DATA
+
+set.seed(1234)
+
+fractionOfDataForTraining = 0.7 #70% train and 30% test data. 
+
+partitionData <- function( data, fractionOfDataForTraining){
+  numberOfRows <- nrow( data )
+  randomRows   <- runif(numberOfRows)
+  flag         <- randomRows <= fractionOfDataForTraining
+  trainData <- data[ flag, ]
+  testData  <- data[ !flag, ]
+  dataSetSplit <- list( trainData = trainData, testData = testData )
+  dataSetSplit
+}
+
+#partition for mushroom data for first and second consesus algorithm
+paritionedData <- partitionData(mushrooms_majorityvotinglabels, fractionOfDataForTraining)
+trainData   <- paritionedData$trainData
+testData    <- paritionedData$testData
+
+#partition for golden label majority voted labels, mushroom data
+#TODO
+
+#partition for 2nd data, 2 different consensus algorithms
+#TODO
+
+#partition for 3rd data, 2 different consensus algorithms
+#TODO
+
 
 #-----------------
 
