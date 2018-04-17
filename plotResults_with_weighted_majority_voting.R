713c4e4066cd119ab5c0e68f4e0bdea8dc9d038f
sallav <salla_mari@hotmail.com>
2018-04-17 16:31:13 +0300

    Create plotResults_with_weighted_majority_voting.R

---
+++ plotResults_with_weighted_majority_voting.R
@@ -0,0 +1,264 @@
+## Functions
+
+#SIMULATE NOISY DATA with X% noise FOR N WORKERS. 
+
+#function to simulate noisy labels
+simulateNoisyWorkers <- function(amount_of_workers, amount_of_noise, expertlabel){
+  label_values <- unique(expertlabel)
+  N=length(expertlabel)
+  workers_with_noisy_labels<-matrix(NA,nrow=N, ncol=amount_of_workers)
+  for (i in 1:amount_of_workers) {
+    #generates which labels are noisy for each worker
+    noise <- rbinom(N,1,amount_of_noise) # 0,1,1 etc
+    labels <- expertlabel
+    for(j in 1:N){
+      # 1 == label with noise
+      if(noise[j] == 1){
+        #replace the chosen noisy labels with incorret values
+        wrong_labels_for_row_j <- label_values[which(label_values != expertlabel[j])]
+        labels[j]<- wrong_labels_for_row_j[sample(1:length(wrong_labels_for_row_j),1)]
+      }
+    }
+    #save noisy labels to matrix
+    workers_with_noisy_labels[,i] <- labels
+  }
+  
+  return(workers_with_noisy_labels)
+}
+
+simulateNoisyWorkersMixedPopulation <- function(amount_of_workers, expertlabel){
+  amount_of_workers <- c(0.1,0.3,0.3,0.3)*amount_of_workers
+  amount_of_noise <- c(0.05,0.2,0.4,0.75)
+  
+  mixed_population <- NA
+  t_experts <- simulateNoisyWorkers(amount_of_workers[1],amount_of_noise[1],expertlabel)
+  experts <- simulateNoisyWorkers(amount_of_workers[2],amount_of_noise[2],expertlabel)
+  mixed_population <- cbind(t_experts, experts)
+  amateurs <- simulateNoisyWorkers(amount_of_workers[2],amount_of_noise[3],expertlabel)
+  mixed_population <- cbind(mixed_population, amateurs)
+  adversasiers <- generateRandomLabels(amount_of_workers[4],expertlabel)
+  mixed_population <- cbind(mixed_population, adversasiers)
+  
+  return(mixed_population)
+}
+
+
+#------------------------------
+
+# Algorithm for weighted majority voting
+weighted_majority_voting <- function(labelset)
+{
+  # all labels given by different workers in matrix
+  dataset <- labelset
+  rows <- length(dataset[,1])
+  cols <- length(dataset[1,])
+  first <- dataset[1, 1]
+  datatype <- typeof(first)
+  
+  # vector for final labels (consensus labels)
+  labels <- vector(datatype, length = rows)
+  # vector of zeros is initialised for worker scores
+  scores <- vector("integer", length = cols)
+  # vector for all different possible labels
+  labelspace <- vector(datatype, length = 1)
+  labelspace[1] <- first
+  
+  # library for general majority voting
+  library(mclust)
+  
+  # score workers based on agreement with majority
+  for (i in 1:rows){
+    # get majority vote   
+    freq <- (majorityVote(dataset[i,]))$majority
+    for (j in 1:cols){
+      currentlabel <- dataset[i, j]
+      # see if worker voted for the most frequent label
+      if (identical(toString(currentlabel), freq)){
+        # increment score if true
+        scores[j] = scores[j] + 1
+      }
+      # collect unseen labels to labelspace vector
+      if(!(is.element(currentlabel, labelspace))){
+        append(labelspace, currentlabel)
+      }
+    }
+  }
+  
+  # copy counted scores
+  sortedscores <- vector("numeric", length = cols)
+  sortedscores <- rep(scores, 1) 
+  # sort scores
+  sort(sortedscores)
+  
+  # choose threshold using appropriate column from sorted scores
+  expert_threshold <- sortedscores[as.integer(4*(cols/10))]
+  # choose another threshold
+  spammer_threshold <- sortedscores[as.integer(cols-(cols/3))]
+  # amount of labels:
+  labelcount <- length(labelspace)
+  
+  # majority vote with weights
+  for (i in 1:rows){
+    # initialise vector for giving scores for labels
+    labelscores <- rep(0, labelcount)
+    for (j in 1:cols){
+      # determine weight based on worker scores and thresholds
+      weight <- 1
+      if(scores[j]<expert_threshold){
+        weight <- weight - 0.4
+      }
+      if(scores[j]<spammer_threshold){
+        weight <- 0
+      }
+      # find label's index in labelspace
+      index <- match(dataset[i, j], labelspace)
+      # increment labels weight pointed by index
+      labelscores[index] = labelscores[index] + weight 
+    }
+    best <- 0
+    freqind <- 1
+    # iterate labels to see which has biggest weight
+    for (k in 1:labelcount){
+      if(labelscores[k]>best){
+        freqind <- k
+        best <- labelscores[k]
+      }
+    }
+    # choose the label with biggest weight as final label
+    labels[i] <- labelspace[freqind]
+  }
+  
+  # return final labels
+  return(labels)
+}
+
+#CONSENSUS ALG 1: MAJOTRITY VOTING to reduce multiple labels
+#install.packages("mclust")
+library(mclust) 
+
+# apply the majority voting to worker labels
+# cbind majority votes to data 
+# data  cant have the original labels anymore
+majorityVotingForLabels <- function(workers_labels,data){
+  # Use Weighted majority voting!
+  labels_majorityvoting <- weighted_majority_voting(workers_labels)
+
+  labels_majorityvoting <- as.factor(labels_majorityvoting)
+  labels_majorityvoting<-cbind(labels_majorityvoting, data)
+  names(labels_majorityvoting)[names(labels_majorityvoting) == "labels_majorityvoting"] = "class" 
+  return(labels_majorityvoting)
+}  
+
+#CONSENSUS ALG 2: USE GOLDEN LABELS to reduce multiple labels with majority voting
+goldenLabelCheckForLabels <- function(workers_labels){
+  #TODO
+  return(NULL)
+}
+
+
+
+#------------------------------
+
+#PARTITION TO TEST AND TRAIN DATA
+
+set.seed(1234)
+
+fractionOfDataForTraining = 0.3 #70% train and 30% test data. 
+
+partitionData <- function( data, fractionOfDataForTraining, expertlabels){
+  data$class <- NULL
+  numberOfRows <- nrow( data )
+  randomRows   <- runif(numberOfRows)
+  #print(randomRows)
+  flag         <- randomRows <= fractionOfDataForTraining
+  trainData <- data[ flag, ]
+  testData  <- data[ !flag, ]
+  dataSetSplit <- list( trainData = trainData, testData = testData, trainDataLabels=expertlabels[flag],testDataLabels=expertlabels[!flag] )
+  dataSetSplit
+}
+
+#-----------------------------------
+
+getAccuracy <- function(original_data, amount_of_workers, noise_level) {
+  N<-nrow(original_data)
+  #head(original_data)
+  data1 <- original_data
+  library(data.table)
+  data1_expertlabels <- data.table(ID=c(1:N), label=c(original_data$class))
+  data1_expertlabels <- data1_expertlabels$label
+  
+  
+  #partition for mushroom data without classes eatable/poisonous
+  paritionedData <- partitionData(data1, fractionOfDataForTraining,data1_expertlabels)
+  trainData   <- paritionedData$trainData
+  testData    <- paritionedData$testData
+  expertlabels_trainData <- paritionedData$trainDataLabels
+  expertlabels_testData <- paritionedData$testDataLabels
+  
+  #Add noise to train data labels
+  
+  #simulate multiple labels with noive level x%
+  data1_workers_labels_testData <-simulateNoisyWorkers(amount_of_workers,noise_level,expertlabels_testData)
+  #data1_workers_labels_testData <- simulateNoisyWorkersMixedPopulation(amount_of_workers,expertlabels_testData)
+  
+  #majorityvoting for noisy labels
+  data1_labels_majorityvoting <- majorityVotingForLabels(data1_workers_labels_testData, testData)
+  testData <- data1_labels_majorityvoting
+  
+  #calculate the amount of noise in majority voted labels
+  res <- data1_labels_majorityvoting$class == expertlabels_testData
+  length(res[res==TRUE])/(length(res))
+  
+  
+  #RUN 3 DIFFERENT CLASSIFICATION ALGORITHMS
+  #install.packages("e1071")
+  library(e1071)
+  
+  #Fit a model and support vector machine prediction
+  schema <- class ~ .
+  nrow(trainData)
+  nrow(testData)
+  model_svm <- svm(schema , testData)
+  pred <- predict(model_svm, trainData)
+  
+  #calculate correct predictions  
+  res <- as.numeric(pred) == expertlabels_trainData
+  length(res[res==TRUE])/(length(res))
+}
+
+
+# load spam data
+# original_data <- read.csv('data_spambase.txt')
+# original_data$class <- as.factor(original_data$X1)
+# original_data$X1 <- NULL
+
+# load mushroom data
+# original_data <- read.csv("mushroomsKAGGLE.csv")
+
+# load tic-tac-toe data
+original_data <- read.csv('tic_tac_toe_game.txt')
+original_data$class <- as.factor(original_data$positive)
+original_data$positive <- NULL
+
+amount_of_workers <- c(1, 5, 10, 15, 20, 25, 30)
+noise_level <- c(0.1, 0.2, 0.3, 0.4, 0.5) # x% amount incorret labels
+results <- matrix(nrow=7, ncol = 5)
+
+for (r in 1:length(amount_of_workers))
+  for (c in 1:length(noise_level))
+    results[[r, c]] <- getAccuracy(original_data, amount_of_workers[r], noise_level[c])
+
+results
+
+
+#Plot the predictions and the plot to see our model fit
+par(mar=c(5.1, 4.1, 4.1, 8.1), xpd=TRUE)
+plot(0,0,xlab="workers", ylab="accuracy",xlim = c(0,35),ylim = c(0.3,1),type = "n")
+cl <- rainbow(7)
+
+for (r in 1:nrow(results))
+  lines(amount_of_workers, results[,r], col = cl[r])
+
+#par(xpd=TRUE)
+legend("topright", inset=c(-0.3,0), legend=c("10%", "20%","30%","40%","50%"),
+       col=cl, lty=1:2, cex=0.8)
